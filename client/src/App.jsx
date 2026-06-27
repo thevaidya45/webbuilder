02a726682d78077cdcbdbd05c381ae22d40cadfa
Ayush vaidya <thevaidya45@gmail.com>
2026-06-27 13:22:14 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import WebsiteEditor from './pages/Editor'
 import LiveSite from './pages/LiveSite'
 import Pricing from './pages/Pricing'
 
-export const serverUrl="http://localhost:8000"
+export const serverUrl="https://siteforge-3iaf.onrender.com"
 function App() {
   useGetCurrentUser()
   const {userData}=useSelector(state=>state.user)
